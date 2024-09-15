a062c752e996be7ccda7d6a45dc638ad1e0ab7ef
iaminov <iaminov@users.noreply.github.com>
2024-09-15 14:30:00 +0000

    feat(networking): add private subnets with NAT gateway support

--- modules/networking/outputs.tf
+++ modules/networking/outputs.tf
@@ -13,6 +13,21 @@ output "private_subnet_ids" {
   value       = aws_subnet.private[*].id
 }
 
+output "private_subnet_ids" {
+  description = "List of IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "private_subnet_cidrs" {
+  description = "List of CIDR blocks of private subnets"
+  value       = aws_subnet.private[*].cidr_block
+}
+
+output "nat_gateway_ip" {
+  description = "The public IP address of the NAT Gateway"
+  value       = aws_eip.nat.public_ip
+}
+
 output "vpc_cidr" {
   description = "CIDR block of the VPC"
   value       = aws_vpc.main.cidr_block
