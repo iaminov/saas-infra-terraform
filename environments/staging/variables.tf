c6a1f36a44804729a05dbb9bc9ec2252012e6ea9
iaminov <iaminov@users.noreply.github.com>
2024-12-21 10:00:00 -0500

    feat(env:staging): add variables (vpc, azs, eks sizing)

---
+++ environments/staging/variables.tf
@@ -0,0 +1,53 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "staging"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.1.0.0/16"
+}
+
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "saas-staging-cluster"
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for the cluster"
+  type        = string
+  default     = "1.28"
+}
+
+variable "instance_types" {
+  description = "List of instance types for the node group"
+  type        = list(string)
+  default     = ["t3.medium"]
+}
+
+variable "desired_size" {
+  description = "Desired number of worker nodes"
+  type        = number
+  default     = 2
+}
+
+variable "max_size" {
+  description = "Maximum number of worker nodes"
+  type        = number
+  default     = 4
+}
+
+variable "min_size" {
+  description = "Minimum number of worker nodes"
+  type        = number
+  default     = 1
+}
